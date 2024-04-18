4e1e4fb6fa29328291dbbd599c3b93384f074980
huddlepe <huddlepe@mail.uc.edu>
2024-04-18 13:38:20 -0400

    patience's hard problem commit

--- Group3_FinalProject/HardProblem.aspx
+++ Group3_FinalProject/HardProblem.aspx
@@ -4,11 +4,34 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Hard Problem</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <h2>Select a LeetCode Problem</h2>
+            <asp:DropDownList ID="ddlProblems" runat="server" OnSelectedIndexChanged="ddlProblems_SelectedIndexChanged">
+                <asp:ListItem Text="Easy Problem" Value="1"></asp:ListItem>
+                <asp:ListItem Text="Medium Problem" Value="1"></asp:ListItem>
+                <asp:ListItem Text="Hard Problem" Value="1"></asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:Button ID="btnSolve" runat="server" Text="Solve Problem" OnClick="btnSolve_Click" />
+            <br />
+            <div>
+                <h2>Problem Description</h2>
+                <asp:Label ID="lblDescription" runat="server" Text=""></asp:Label>
+            </div>
+            <br />
+            <div>
+                <h2>Test Case</h2>
+                <asp:Label ID="lblTestCase" runat="server" Text=""></asp:Label>
+            </div>
+            <br />
+            <div>
+                <h2>Solution</h2>
+                <asp:Label ID="lblSolution" runat="server" Text=""></asp:Label>
+            </div>
         </div>
     </form>
 </body>
--- Group3_FinalProject/HardProblem.aspx.cs
+++ Group3_FinalProject/HardProblem.aspx.cs
@@ -11,6 +11,65 @@ namespace Group3_FinalProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                ddlProblems.Items.Add(new ListItem("Hard Problem: Distribute Elements Equally"));
+            }
+        }
+
+        protected void btnSolve_Click(object sender, EventArgs e)
+        {
+            if (ddlProblems.SelectedIndex == 0)
+            {
+                int[] nums = { 3, 4, 1, 2, 7, 5, 6 };
+                int[] result = DistributeElements(nums);
+                lblSolution.Text = string.Join(", ", result);
+            }
+        }
+
+        public int[] DistibutionElements(int[] nums)
+        {
+            int n = nums.Length;
+            List<int> arr1 = new List<int>();
+            List<int> arr2 = new List<int>();
+
+            arr1.Add(nums[0]);
+
+            for (int i = 1; i < n; i++)
+            {
+                int count1 = GreaterCount(arr1.ToArray(), nums[i]);
+                int count2 = GreaterCount(arr2.ToArray(), nums[i]);
+
+                if (count1 > count2 || (count1 == count2 && arr1.Count <= arr2.Count))
+                {
+                    arr1.Add(nums[i]);
+                }
+                else
+                {
+                    arr2.Add(nums[i]);
+                }
+            }
+
+            List<int> result = new List<int>(arr1);
+            result.AddRange(arr2);
+            return result.ToArray();
+        }
+
+        public int GreaterCount(int[] arr, int val)
+        {
+            int count = 0;
+            foreach (int num in arr)
+            {
+                if (num > val)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        protected System.Void ddlProblems_SelectedIndexChanged()
+        {
 
         }
     }
